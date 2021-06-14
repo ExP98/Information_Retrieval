a0786a0223dac62d8998cad681aa4133cace0258
Egor Glushkov <egorglushkov2014@yandex.ru>
2021-06-14 10:02:51 +0300

    not query

--- src/query_processing.py
+++ src/query_processing.py
@@ -7,7 +7,9 @@ index_file = "../index/index_stemming.pkl"
 def single_word_query():
     inverted_index = get_inverted_index(index_file)
     while True:
-        query = input("Enter one word as a query: ")
+        query = input("\nTo exit write exit()\nEnter one word as a query: ")
+        if query == "exit()":
+            break
         if len(query.split()) > 1:
             print("This is not a single word")
             break
@@ -26,7 +28,8 @@ def single_word_query():
 def multiple_and_word_query():
     inverted_index = get_inverted_index(index_file)
     while True:
-        query = input("\nAND query (without AND, list the words separated by spaces): ")
+        query = input("\nTo exit write exit()\n"
+                      "AND query (without AND, list the words separated by spaces): ")
         if query == "exit()":
             break
         terms = [word_stemming(w) for w in query.split()]
@@ -45,7 +48,8 @@ def multiple_and_word_query():
 def multiple_or_word_query():
     inverted_index = get_inverted_index(index_file)
     while True:
-        query = input("\nOR query (without OR, list the words separated by spaces): ")
+        query = input("\nTo exit write exit()"
+                      "\nOR query (without OR, list the words separated by spaces): ")
         if query == "exit()":
             break
         terms = [word_stemming(w) for w in query.split()]
@@ -67,7 +71,10 @@ def not_word_query():
     all_doc_id_set = set(arts.keys())
 
     while True:
-        query = input("\nNOT query (without NOT, list the words separated by spaces): ")
+        query = input("\nTo exit write exit()"
+                      "\nNOT query (without NOT, list the words separated by spaces): ")
+        if query == "exit()":
+            break
         if len(query.split()) > 1:
             print("This is not a single word")
             break
@@ -84,7 +91,69 @@ def not_word_query():
             # print('doc IDs: ', diff_id)
 
 
+def get_ind(l, elem):
+    if l.__contains__(elem):
+        ind = l.index(elem)
+    else:
+        ind = None
+    return ind
+
+
+def parsing_query():
+    inverted_index = get_inverted_index(index_file)
+
+    with open('../index/all_articles.pkl', 'rb') as load_file:
+        arts = pickle.load(load_file, encoding='latin1')
+    load_file.close()
+    all_doc_id_set = set(arts.keys())
+
+    oper = ["AND", "NOT", "OR"]
+
+    while True:
+        query_string = input("\nTo exit write exit()\nQuery: ")
+        if query_string == "exit()":
+            break
+
+        q_list = query_string.split()
+        for i, elem in enumerate(q_list):
+            if elem not in oper:
+                q = word_stemming(elem)
+                q_list[i] = set(x[0] for x in inverted_index[q])
+
+        ind = get_ind(q_list, "NOT")
+        while ind:
+            tmp = all_doc_id_set - q_list[ind + 1]
+            q_list.pop(ind)
+            q_list[ind] = tmp
+            ind = get_ind(q_list, "NOT")
+
+        ind = get_ind(q_list, "AND")
+        while ind:
+            tmp = q_list[ind - 1] & q_list[ind + 1]
+            q_list.pop(ind)
+            q_list.pop(ind)
+            q_list[ind - 1] = tmp
+            ind = get_ind(q_list, "AND")
+
+        ind = get_ind(q_list, "OR")
+        while ind:
+            tmp = q_list[ind - 1] | q_list[ind + 1]
+            q_list.pop(ind)
+            q_list.pop(ind)
+            q_list[ind - 1] = tmp
+            ind = get_ind(q_list, "OR")
+        print('Count of docs: ', len(q_list[0]))
+        print('doc IDs: ', q_list[0])
+
+
+
+
+# parsing_query('Moscow OR Berlin OR Rome')
+# parsing_query('health AND care AND aids')
+# parsing_query('health AND care OR aids')
+parsing_query()
+
 # single_word_query()
 # multiple_and_word_query()           # for example (without quotes): "health care aids"
 # multiple_or_word_query()
-not_word_query()
+# not_word_query()
